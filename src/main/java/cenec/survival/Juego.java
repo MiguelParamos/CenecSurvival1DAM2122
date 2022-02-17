7fb4f8b0206dea98bd2dda2e04a65370aa7c4fca
Miguel Páramos <miguel.paramos@cenecmalaga.es>
2022-02-17 13:24:30 +0100

    Clase Personaje hecha
    Tenemos  constructor, getters y setters

--- src/main/java/cenec/survival/Juego.java
+++ src/main/java/cenec/survival/Juego.java
@@ -1,8 +1,15 @@
 package cenec.survival;
 
+import clases.Arma;
+import clases.Personaje;
+
 public class Juego {
 
 	public static void main(String[] args) {
+		Personaje p=new Personaje("Paco",(byte)39,null,null);
+		System.out.println(p);
+		Personaje k=new Personaje("Felpudoman123",null,null);
+		System.out.println(k);
 		
 	}
 
