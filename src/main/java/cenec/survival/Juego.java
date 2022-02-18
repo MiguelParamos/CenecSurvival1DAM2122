356958d8fc88b14de153a0198b7a60ec0cb100d2
Alexgar98 <92014559+Alexgar98@users.noreply.github.com>
2022-02-18 13:52:25 +0100

    Cambios
    Se había perdido lo mío .-.

--- src/main/java/cenec/survival/Juego.java
+++ src/main/java/cenec/survival/Juego.java
@@ -16,7 +16,7 @@ public class Juego {
 		armasPosibles.add(new Arma("Cuchara de Palo",(byte)20));
 		armasPosibles.add(new Arma("Botijo nuclear",(byte)17));
 		armasPosibles.add(new Arma("Zapatilla voladora",(byte)20));
-		
+		armasPosibles.add(new Arma("Alpargata",(byte)20));
 		armasPosibles.add(new Arma("teclado custom",(byte)13));
 		
 		
@@ -26,7 +26,7 @@ public class Juego {
 		proteccionesPosibles.add(new Proteccion("Pato de Goma",(byte)5));
 		proteccionesPosibles.add(new Proteccion("Careta de Pedro Sanchez",(byte)20));
 		proteccionesPosibles.add(new Proteccion("Silla de plastico",(byte)5));
-		
+		proteccionesPosibles.add(new Proteccion("Sustituto",(byte)10));
 		proteccionesPosibles.add(new Proteccion("Refrigeracion liquida",(byte)7));
 		
 		
@@ -35,7 +35,7 @@ public class Juego {
 		Personaje juanmi=new Personaje("Juanmi",armasPosibles.get(r.nextInt(armasPosibles.size())),proteccionesPosibles.get(r.nextInt(proteccionesPosibles.size())));
 		Personaje eric=new Personaje("Eric",armasPosibles.get(r.nextInt(armasPosibles.size())),proteccionesPosibles.get(r.nextInt(proteccionesPosibles.size())));
 		Personaje miguel=new Personaje("Miguel P�ramos",armasPosibles.get(r.nextInt(armasPosibles.size())),proteccionesPosibles.get(r.nextInt(proteccionesPosibles.size())));
-		
+		Personaje alexis=new Personaje("Alexis",armasPosibles.get(r.nextInt(armasPosibles.size())),proteccionesPosibles.get(r.nextInt(proteccionesPosibles.size())));
 		Personaje juan=new Personaje("Juan",armasPosibles.get(r.nextInt(armasPosibles.size())),proteccionesPosibles.get(r.nextInt(proteccionesPosibles.size())));
 
 		
