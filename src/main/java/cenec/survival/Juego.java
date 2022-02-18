77a1ba5676799e71f4409ecaab777322122eef1b
Jeropis <jerocaru@gmail.com>
2022-02-18 13:46:50 +0100

    Añadidas arma, proteccion y Personaje
    añadida arma nueva, protección nueva y personaje nuevo

--- src/main/java/cenec/survival/Juego.java
+++ src/main/java/cenec/survival/Juego.java
@@ -15,20 +15,22 @@ public class Juego {
 		armasPosibles.add(new Arma("MataCarpinchos",(byte)15));
 		armasPosibles.add(new Arma("Cuchara de Palo",(byte)20));
 		armasPosibles.add(new Arma("Botijo nuclear",(byte)17));
-		
+		armasPosibles.add(new Arma("Mechero de la Sole",(byte)8));
 		
 		
 		ArrayList<Proteccion> proteccionesPosibles=new ArrayList<Proteccion>();
 		proteccionesPosibles.add(new Proteccion("Carpincho",(byte)20)); 
 		proteccionesPosibles.add(new Proteccion("Pato de Goma",(byte)5));
 		proteccionesPosibles.add(new Proteccion("Careta de Pedro Sanchez",(byte)20));
+		proteccionesPosibles.add(new Proteccion("Declaracion de la Renta",(byte)11));
 		
 		
 		Random r=new Random();
 		Personaje juanmi=new Personaje("Juanmi",armasPosibles.get(r.nextInt(armasPosibles.size())),proteccionesPosibles.get(r.nextInt(proteccionesPosibles.size())));
 		Personaje eric=new Personaje("Eric",armasPosibles.get(r.nextInt(armasPosibles.size())),proteccionesPosibles.get(r.nextInt(proteccionesPosibles.size())));
 		Personaje miguel=new Personaje("Miguel Páramos",armasPosibles.get(r.nextInt(armasPosibles.size())),proteccionesPosibles.get(r.nextInt(proteccionesPosibles.size())));
-		Personaje[] personajesPosibles= {juanmi,eric,miguel};
+		Personaje jero=new Personaje("Jero",armasPosibles.get(r.nextInt(armasPosibles.size())),proteccionesPosibles.get(r.nextInt(proteccionesPosibles.size())));
+		Personaje[] personajesPosibles= {juanmi,eric,miguel,jero};
 		
 
 		
