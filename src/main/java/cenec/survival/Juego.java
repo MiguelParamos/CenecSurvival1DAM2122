afd45424f406eea6af40bf23c1445f8be937d472
ale <alejandrobarrionuevo00@gmail.com>
2022-02-18 13:43:05 +0100

    cambio

--- src/main/java/cenec/survival/Juego.java
+++ src/main/java/cenec/survival/Juego.java
@@ -11,30 +11,33 @@ import clases.Proteccion;
 public class Juego {
 
 	public static void main(String[] args) {
-		ArrayList<Arma> armasPosibles=new ArrayList<Arma>();
-		armasPosibles.add(new Arma("MataCarpinchos",(byte)15));
-		armasPosibles.add(new Arma("Cuchara de Palo",(byte)20));
-		armasPosibles.add(new Arma("Botijo nuclear",(byte)17));
-		
-		
-		
-		ArrayList<Proteccion> proteccionesPosibles=new ArrayList<Proteccion>();
-		proteccionesPosibles.add(new Proteccion("Carpincho",(byte)20)); 
-		proteccionesPosibles.add(new Proteccion("Pato de Goma",(byte)5));
-		proteccionesPosibles.add(new Proteccion("Careta de Pedro Sanchez",(byte)20));
-		
-		
-		Random r=new Random();
-		Personaje juanmi=new Personaje("Juanmi",armasPosibles.get(r.nextInt(armasPosibles.size())),proteccionesPosibles.get(r.nextInt(proteccionesPosibles.size())));
-		Personaje eric=new Personaje("Eric",armasPosibles.get(r.nextInt(armasPosibles.size())),proteccionesPosibles.get(r.nextInt(proteccionesPosibles.size())));
-		Personaje miguel=new Personaje("Miguel Páramos",armasPosibles.get(r.nextInt(armasPosibles.size())),proteccionesPosibles.get(r.nextInt(proteccionesPosibles.size())));
-		Personaje[] personajesPosibles= {juanmi,eric,miguel};
-		
+		ArrayList<Arma> armasPosibles = new ArrayList<Arma>();
+		armasPosibles.add(new Arma("MataCarpinchos", (byte) 15));
+		armasPosibles.add(new Arma("Cuchara de Palo", (byte) 20));
+		armasPosibles.add(new Arma("Botijo nuclear", (byte) 17));
+		armasPosibles.add(new Arma("Palillo de oido", (byte) 20));
 
-		
-		Combate pelea1=new Combate(juanmi,eric);
+		ArrayList<Proteccion> proteccionesPosibles = new ArrayList<Proteccion>();
+		proteccionesPosibles.add(new Proteccion("Carpincho", (byte) 20));
+		proteccionesPosibles.add(new Proteccion("Pato de Goma", (byte) 5));
+		proteccionesPosibles.add(new Proteccion("Careta de Pedro Sanchez", (byte) 20));
+		proteccionesPosibles.add(new Proteccion("Escudo de papel", (byte) 20));
+
+		Random r = new Random();
+		Personaje juanmi = new Personaje("Juanmi", armasPosibles.get(r.nextInt(armasPosibles.size())),
+				proteccionesPosibles.get(r.nextInt(proteccionesPosibles.size())));
+		Personaje eric = new Personaje("Eric", armasPosibles.get(r.nextInt(armasPosibles.size())),
+				proteccionesPosibles.get(r.nextInt(proteccionesPosibles.size())));
+		Personaje miguel = new Personaje("Miguel Pï¿½ramos", armasPosibles.get(r.nextInt(armasPosibles.size())),
+				proteccionesPosibles.get(r.nextInt(proteccionesPosibles.size())));
+		Personaje aleB = new Personaje("Ale Barrionuevo", armasPosibles.get(r.nextInt(armasPosibles.size())),
+				proteccionesPosibles.get(r.nextInt(proteccionesPosibles.size())));
+
+		Personaje[] personajesPosibles = { juanmi, eric, miguel };
+
+		Combate pelea1 = new Combate(juanmi, eric);
 		pelea1.combatir();
-		
+
 	}
 
 }
