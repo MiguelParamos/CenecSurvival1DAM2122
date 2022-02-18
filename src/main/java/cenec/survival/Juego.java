23df58dbcad1a5bc39d17db29ddaa9c471d0654d
Miguel Páramos <miguel.paramos@cenecmalaga.es>
2022-02-18 13:59:36 +0100

    Intento de arreglar la oleada de conflictos
    Listos para la ronda 2

--- src/main/java/cenec/survival/Juego.java
+++ src/main/java/cenec/survival/Juego.java
@@ -11,72 +11,48 @@ import clases.Proteccion;
 public class Juego {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
 		ArrayList<Arma> armasPosibles = new ArrayList<Arma>();
 		armasPosibles.add(new Arma("MataCarpinchos", (byte) 15));
 		armasPosibles.add(new Arma("Cuchara de Palo", (byte) 20));
 		armasPosibles.add(new Arma("Botijo nuclear", (byte) 17));
 		armasPosibles.add(new Arma("Palillo de oido", (byte) 20));
 		armasPosibles.add(new Arma("Chusta de canuto", (byte)15));
-
+		armasPosibles.add(new Arma("Zapatilla voladora",(byte)20));
+		armasPosibles.add(new Arma("Alpargata",(byte)20));
+		armasPosibles.add(new Arma("teclado custom",(byte)13));
+		
+		
 		ArrayList<Proteccion> proteccionesPosibles = new ArrayList<Proteccion>();
 		proteccionesPosibles.add(new Proteccion("Carpincho", (byte) 20));
 		proteccionesPosibles.add(new Proteccion("Pato de Goma", (byte) 5));
 		proteccionesPosibles.add(new Proteccion("Careta de Pedro Sanchez", (byte) 20));
 		proteccionesPosibles.add(new Proteccion("Escudo de papel", (byte) 20));
 		proteccionesPosibles.add(new Proteccion("Litro vacia", (byte)17));
-
-		Random r = new Random();
-		Personaje juanmi = new Personaje("Juanmi", armasPosibles.get(r.nextInt(armasPosibles.size())),
-				proteccionesPosibles.get(r.nextInt(proteccionesPosibles.size())));
-		Personaje eric = new Personaje("Eric", armasPosibles.get(r.nextInt(armasPosibles.size())),
-				proteccionesPosibles.get(r.nextInt(proteccionesPosibles.size())));
-		Personaje miguel = new Personaje("Miguel Pï¿½ramos", armasPosibles.get(r.nextInt(armasPosibles.size())),
-				proteccionesPosibles.get(r.nextInt(proteccionesPosibles.size())));
-		Personaje aleB = new Personaje("Ale Barrionuevo", armasPosibles.get(r.nextInt(armasPosibles.size())),
-				proteccionesPosibles.get(r.nextInt(proteccionesPosibles.size())));
-		Personaje joaquin = new Personaje("Joaquin Moreno", armasPosibles.get(r.nextInt(armasPosibles.size())), 
-				proteccionesPosibles.get(r.nextInt(proteccionesPosibles.size())));
-
-		Personaje[] personajesPosibles = { juanmi, eric, miguel };
-
-		Combate pelea1 = new Combate(juanmi, eric);
-=======
-		ArrayList<Arma> armasPosibles=new ArrayList<Arma>();
-		armasPosibles.add(new Arma("MataCarpinchos",(byte)15));
-		armasPosibles.add(new Arma("Cuchara de Palo",(byte)20));
-		armasPosibles.add(new Arma("Botijo nuclear",(byte)17));
-		armasPosibles.add(new Arma("Zapatilla voladora",(byte)20));
-		armasPosibles.add(new Arma("Alpargata",(byte)20));
-		armasPosibles.add(new Arma("teclado custom",(byte)13));
-		
-		
-		
-		ArrayList<Proteccion> proteccionesPosibles=new ArrayList<Proteccion>();
-		proteccionesPosibles.add(new Proteccion("Carpincho",(byte)20)); 
-		proteccionesPosibles.add(new Proteccion("Pato de Goma",(byte)5));
-		proteccionesPosibles.add(new Proteccion("Careta de Pedro Sanchez",(byte)20));
 		proteccionesPosibles.add(new Proteccion("Silla de plastico",(byte)5));
 		proteccionesPosibles.add(new Proteccion("Sustituto",(byte)10));
 		proteccionesPosibles.add(new Proteccion("Refrigeracion liquida",(byte)7));
 		
 		
-		Random r=new Random();
-		
-		Personaje juanmi=new Personaje("Juanmi",armasPosibles.get(r.nextInt(armasPosibles.size())),proteccionesPosibles.get(r.nextInt(proteccionesPosibles.size())));
-		Personaje eric=new Personaje("Eric",armasPosibles.get(r.nextInt(armasPosibles.size())),proteccionesPosibles.get(r.nextInt(proteccionesPosibles.size())));
-		Personaje miguel=new Personaje("Miguel Pï¿½ramos",armasPosibles.get(r.nextInt(armasPosibles.size())),proteccionesPosibles.get(r.nextInt(proteccionesPosibles.size())));
+		Random r = new Random();
+		Personaje juanmi = new Personaje("Juanmi", armasPosibles.get(r.nextInt(armasPosibles.size())),
+				proteccionesPosibles.get(r.nextInt(proteccionesPosibles.size())));
+		Personaje eric = new Personaje("Eric", armasPosibles.get(r.nextInt(armasPosibles.size())),
+				proteccionesPosibles.get(r.nextInt(proteccionesPosibles.size())));
+		Personaje miguel = new Personaje("Miguel Páramos", armasPosibles.get(r.nextInt(armasPosibles.size())),
+				proteccionesPosibles.get(r.nextInt(proteccionesPosibles.size())));
+		Personaje aleB = new Personaje("Ale Barrionuevo", armasPosibles.get(r.nextInt(armasPosibles.size())),
+				proteccionesPosibles.get(r.nextInt(proteccionesPosibles.size())));
+		Personaje joaquin = new Personaje("Joaquin Moreno", armasPosibles.get(r.nextInt(armasPosibles.size())), 
+				proteccionesPosibles.get(r.nextInt(proteccionesPosibles.size())));
 		Personaje alexis=new Personaje("Alexis",armasPosibles.get(r.nextInt(armasPosibles.size())),proteccionesPosibles.get(r.nextInt(proteccionesPosibles.size())));
 		Personaje juan=new Personaje("Juan",armasPosibles.get(r.nextInt(armasPosibles.size())),proteccionesPosibles.get(r.nextInt(proteccionesPosibles.size())));
 
+		Personaje[] personajesPosibles = { juanmi, eric, miguel,aleB,joaquin,alexis,juan };
 		
-		Personaje juanmi1=new Personaje("Juanmi",armasPosibles.get(r.nextInt(armasPosibles.size())),proteccionesPosibles.get(r.nextInt(proteccionesPosibles.size())));
-		Personaje[] personajesPosibles= {juanmi,juanmi1, eric,miguel,juan};
 		
-
 		
 		Combate pelea1=new Combate(juanmi,eric);
->>>>>>> 3afc6557f8cdd61e52508d25469348512cabd512
+		
 		pelea1.combatir();
 
 	}
