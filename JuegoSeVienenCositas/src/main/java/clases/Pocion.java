a1a3e316151dc504e36537e3848ca04b7c66540a
ale <alejandrobarrionuevo00@gmail.com>
2022-02-25 13:45:31 +0100

    posicion3

--- JuegoSeVienenCositas/src/main/java/clases/Pocion.java
+++ JuegoSeVienenCositas/src/main/java/clases/Pocion.java
@@ -1,12 +1,14 @@
 package clases;
 
+import superclases.Objeto;
+
 /**
  * Representa un objeto que sumara la vida actual del personaje.
  * 
  * @author Alejandro
  * 
  */
-public class Pocion {
+public class Pocion extends Objeto {
     /**
      * cantidadPuntosRestaurados --> Representa la cantidad de punto que sumará la
      * vida del personaje
