c7aaea09e04f86dc32705996861a9039eb405316
gonzalotroya <92518256+gonzalotroya@users.noreply.github.com>
2022-02-25 13:40:55 +0100

    jugadorcomentado

--- JuegoSeVienenCositas/src/main/java/clases/Jugador.java
+++ JuegoSeVienenCositas/src/main/java/clases/Jugador.java
@@ -1,6 +1,13 @@
 package clases;
-
+/**
+ * Representa un jugador que cuenta con unas posiciones que se usaran para moverse y localizarlo.
+ * @author Gonzalo de Troya
+ * 
+ */
 public class Jugador {
+	/**
+	 * Representa la posicion de fila y de columna que tendran en el tablero.
+	 */
 	private byte posicionFila;
 	private byte posicionCol;
 
