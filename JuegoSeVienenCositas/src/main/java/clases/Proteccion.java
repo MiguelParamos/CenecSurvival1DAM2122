c9fb4038c8c35b8789031f52991d932bb7be7fa3
HugoMM115 <hugomm935@gmail.com>
2022-02-25 14:22:13 +0100

    Update Proteccion.java

--- JuegoSeVienenCositas/src/main/java/clases/Proteccion.java
+++ JuegoSeVienenCositas/src/main/java/clases/Proteccion.java
@@ -11,4 +11,29 @@ import superclases.Objeto;
 public class Proteccion extends Objeto{
 	/** Representan los puntos de defensa de los personajes */
 	private byte puntosDefensa;
+
+
+	/**
+	 * Constructor de puntos de defensa
+	 * @param puntosDefensa
+	 */
+	public Proteccion(byte puntosDefensa) {
+		super();
+		this.puntosDefensa = puntosDefensa;
+	}
+/**
+ * getter de Puntos de defensa
+ * @return puntos de defensa de la clase proteccion
+ */
+	public byte getPuntosDefensa() {
+		return puntosDefensa;
+	}
+/**
+ * setter de puntos de defensa
+ * @param puntosDefensa de la clase Proteccion
+ */
+	public void setPuntosDefensa(byte puntosDefensa) {
+		this.puntosDefensa = puntosDefensa;
+	}
+	
 }
