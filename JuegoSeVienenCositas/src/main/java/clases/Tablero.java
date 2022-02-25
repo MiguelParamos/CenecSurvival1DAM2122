49b2125c30ebf159ac893173d28e0a65a4f7aff2
Marcos-Garcia-Medina <74043202+Marcos-Garcia-Medina@users.noreply.github.com>
2022-02-25 13:41:41 +0100

    Tablero Documentado y Con Extends Hecho
    Tablero Documentado y Con Extends Hecho

--- JuegoSeVienenCositas/src/main/java/clases/Tablero.java
+++ JuegoSeVienenCositas/src/main/java/clases/Tablero.java
@@ -1,6 +1,15 @@
 package clases;
 
-public class Tablero {
+import superclases.EntidadConNombre;
+/**
+ * Representa una matriz de lugar, que tendra 16 posiciones (4x4), y un jugador, el cual se
+ * usara para jugar.
+ * @author Marcos Garcia Medina
+ *
+ */
+public class Tablero extends EntidadConNombre {
+	/**	lugares representa una matriz de tipo Lugar con 16 lugares diferentes.	**/
 	private Lugar[][] lugares;
+	/**	personaje representa un jugador de tipo Jugador, el cual se usara para jugar.	**/
 	private Jugador personaje;
 }
