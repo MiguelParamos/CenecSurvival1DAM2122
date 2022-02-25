80b4d1f02495c50fa28fd6747efb88590eee5c12
Jeropis <jerocaru@gmail.com>
2022-02-25 14:04:02 +0100

    Actualizada la clase EntidadConMonedas

--- JuegoSeVienenCositas/src/main/java/superclases/EntidadConMonedas.java
+++ JuegoSeVienenCositas/src/main/java/superclases/EntidadConMonedas.java
@@ -9,4 +9,32 @@ package superclases;
 public class EntidadConMonedas extends EntidadConNombre {
 	/**Representa un objeto que lleva el personaje con el que puede comprar cosas (armas, proteccion o pociones)**/
 	private byte dinero;
+
+	/**
+	 * Constructor de una entidad con monedas 
+	 * @param dinero el numero de dinero que tendrá la entidad
+	 */
+	
+	public EntidadConMonedas(byte dinero) {
+		super();
+		this.dinero = dinero;
+	}
+
+	/**
+	 * Getter de dinero
+	 * @return El dinero que tiene la entidad
+	 */
+	
+	public byte getDinero() {
+		return dinero;
+	}
+	/**
+	 * Setter de dinero
+	 * @param dinero Especificas el dinero que tendrá la entidad
+	 */
+	public void setDinero(byte dinero) {
+		this.dinero = dinero;
+	}
+	
+	
 }
