f9f387a6107aaebb811f619e510ea09de8a74d0f
Miguel Páramos <miguel.paramos@cenecmalaga.es>
2022-02-25 14:38:54 +0100

    Arreglados enemigo y jugador

--- JuegoSeVienenCositas/src/main/java/clases/Enemigo.java
+++ JuegoSeVienenCositas/src/main/java/clases/Enemigo.java
@@ -7,4 +7,19 @@ package clases;
 public class Enemigo extends Personaje {
 	/**	Representa la posiblidad de que el enemigo que salga sea el boss final o no */
 	private boolean esFinal;
+
+	public Enemigo(String nombre, byte dinero, short vida, Arma arma, Proteccion proteccion, boolean esFinal) {
+		super(nombre, dinero, vida, arma, proteccion);
+		this.esFinal = esFinal;
+	}
+
+	public boolean isEsFinal() {
+		return esFinal;
+	}
+
+	public void setEsFinal(boolean esFinal) {
+		this.esFinal = esFinal;
+	}
+	
+	
 }
--- JuegoSeVienenCositas/src/main/java/se/vienen/cositas/main/Juego.java
+++ JuegoSeVienenCositas/src/main/java/se/vienen/cositas/main/Juego.java
@@ -2,13 +2,13 @@ package se.vienen.cositas.main;
 
 import clases.Arma;
 import clases.Proteccion;
+import superclases.EntidadConNombre;
+import superclases.Objeto;
 
 public class Juego {
 
 	public static void main(String[] args) {
-		Arma espada=new Arma("Espada",(byte)30,(byte)12);
-		Proteccion escudo=new Proteccion("Escudo",(byte)30,(byte)12);
-		Objeto p=new Objeto("aa",(byte)55);
+		
 	}
 
 }
